a63aea19f7cc26143fba7a8ad9f07ec1e4e0adc3
Eric Barefoot <eabarefoot@gmail.com>
2019-09-17 11:16:14 -0400

    style: rearranging white space to be readable.

--- run_pyDeltaRCM.py
+++ run_pyDeltaRCM.py
@@ -2,12 +2,13 @@ from pyDeltaRCM import BmiDelta
 import numpy as np
 
 if __name__ == '__main__':
+    
     delta = BmiDelta()
+    
     delta.initialize('deltaRCM.yaml')
-
-
+    
     for time in range(0,5000):
         print('Time:', delta.get_current_time())
         delta.update()
-
+        
     delta.finalize()
