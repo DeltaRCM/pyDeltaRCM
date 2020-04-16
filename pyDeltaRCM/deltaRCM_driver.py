b48ec16ece7c24a87ec57f190bbddc0a71a59e54
Eric Barefoot <eabarefoot@gmail.com>
2020-04-16 12:19:50 -0500

    feat: separate BMI wrapper from deltaRCM model
    In this commit, I went through. and tried to find all the things in the 
    core model that are wrapped and accessed by the BMI wrapper. In the 
    process, I tried to take things that didn't need to be in the wrapper, 
    and instead put them in the main model. The biggest thing like this that 
    I found were the model time, which is critical for the model, but was 
    defined both in the BMI wrapper and in the core model itself. The other 
    thing was a list of input and output variable names, that is used to 
    parse the yaml file that specifies the run. 
    
    I moved all these things into the core model, under the premise that if 
    the model is instantiated from a BMI framework, it will acquire those 
    processes once it is initialized, and doesn't need them before 
    initialization.

--- pyDeltaRCM/deltaRCM_driver.py
+++ pyDeltaRCM/deltaRCM_driver.py
@@ -5,7 +5,23 @@ from .deltaRCM_tools import Tools
 import datetime
 
 class pyDeltaRCM(Tools):
-    
+
+    _input_var_names = (
+        'channel_exit_water_flow__speed',
+        'channel_exit_water_x-section__depth',
+        'channel_exit_water_x-section__width',
+        'channel_exit_water_sediment~bedload__volume_fraction',
+        'channel_exit_water_sediment~suspended__mass_concentration',
+        'sea_water_surface__rate_change_elevation',
+        'sea_water_surface__mean_elevation',
+         )
+
+    _output_var_names = (
+        'sea_water_surface__elevation',
+        'sea_water__depth',
+        'sea_bottom_surface__elevation',
+        )
+
     _input_vars = {
         'model_output__site_prefix': {'name':'site_prefix',
             'type': 'string', 'default': ''},
@@ -100,173 +116,172 @@ class pyDeltaRCM(Tools):
         'basin__opt_stratigraphy': {'name': 'save_strata',
             'type': 'choice', 'default': False}
         }
-        
+
     #############################################
     ################## __init__ #################
     #############################################
-    
+
     def __init__(self, input_file):
         '''
         Creates an instance of the model
-        
+
         Sets the most commonly changed variables here
         Calls functions to set the rest and create the domain (for cleanliness)
         '''
-        
+
         self._time = 0.
         self._time_step = 1.
-        
+
         self.verbose = False
         self.input_file = input_file
-        
+
         self.create_dicts()
         self.set_defaults()
         self.import_file()
-        
+
         self.create_other_variables()
-        
+
         self.init_logger()
-        
+
         self.create_domain()
-        
+
         self.init_subsidence()
         self.init_stratigraphy()
         self.init_output_grids()
-        
+
     #############################################
     ################### update ##################
     #############################################
-    
+
     def update(self):
         '''
         Run the model for one full instance
         '''
-        
+
         self.run_one_timestep()
-        
+
         self.apply_subsidence()
-        
+
         self.finalize_timestep()
         self.record_stratigraphy()
-        
+
         self.output_data()
-        
+
         self._time += self.time_step
-        
+
     #############################################
     ################## finalize #################
     #############################################
-    
+
     def finalize(self):
-        
+
         self.output_strata()
-        
+
         try:
             self.output_netcdf.close()
             if self.verbose:
                 print('Closed output netcdf file.')
         except:
             pass
-            
+
     @property
     def time_step(self):
         """The time step."""
         return self._time_step
-        
+
     @time_step.setter
     def time_step(self, new_dt):
         if new_dt * self.init_Np_sed < 100:
             warnings.warn('Using a very small timestep.')
             warnings.warn('Delta might evolve very slowly.')
-            
+
         self.Np_sed = int(new_dt * self.init_Np_sed)
         self.Np_water = int(new_dt * self.init_Np_water)
-        
+
         if self.toggle_subsidence:
             self.sigma = self.subsidence_mask * self.sigma_max * new_dt
-            
+
         self._time_step = new_dt
-        
+
     @property
     def channel_flow_velocity(self):
         ''' Get channel flow velocity '''
         return self.u0
-        
+
     @channel_flow_velocity.setter
     def channel_flow_velocity(self, new_u0):
         self.u0 = new_u0
         self.create_other_variables()
-        
+
     @property
     def channel_width(self):
         ''' Get channel width '''
         return self.N0_meters
-        
+
     @channel_width.setter
     def channel_width(self, new_N0):
         self.N0_meters = new_N0
         self.create_other_variables()
-        
+
     @property
     def channel_flow_depth(self):
         ''' Get channel flow depth '''
         return self.h0
-        
+
     @channel_flow_depth.setter
     def channel_flow_depth(self, new_d):
         self.h0 = new_d
         self.create_other_variables()
-        
+
     @property
     def sea_surface_mean_elevation(self):
         ''' Get sea surface mean elevation '''
         return self.H_SL
-        
+
     @sea_surface_mean_elevation.setter
     def sea_surface_mean_elevation(self, new_se):
         self.H_SL = new_se
-        
+
     @property
     def sea_surface_elevation_change(self):
         ''' Get rate of change of sea surface elevation, per timestep'''
         return self.SLR
-        
+
     @sea_surface_elevation_change.setter
     def sea_surface_elevation_change(self, new_SLR):
         ''' Set rate of change of sea surface elevation, per timestep'''
         self.SLR = new_SLR
-        
+
     @property
     def bedload_fraction(self):
         ''' Get bedload fraction '''
         return self.f_bedload
-        
+
     @bedload_fraction.setter
     def bedload_fraction(self, new_u0):
         self.f_bedload = new_u0
-        
+
     @property
     def influx_sediment_concentration(self):
         ''' Get influx sediment concentration '''
         return self.C0_percent
-        
+
     @influx_sediment_concentration.setter
     def influx_sediment_concentration(self, new_u0):
         self.C0_percent = new_u0
         self.create_other_variables()
-        
+
     @property
     def sea_surface_elevation(self):
         ''' Get stage '''
         return self.stage
-        
+
     @property
     def water_depth(self):
         ''' Get depth '''
         return self.depth
-        
+
     @property
     def bed_elevation(self):
         ''' Get bed elevation '''
         return self.eta
-        
