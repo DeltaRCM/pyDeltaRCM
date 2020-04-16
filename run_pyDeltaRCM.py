e15de7786f8921a15f8382614704f5d8322d778b
Eric Barefoot <eabarefoot@gmail.com>
2020-04-16 12:49:19 -0500

    fix: update bmi import
    Also change script to only import the core model and only update for one timestep to test for runtime errors
    
    Add output files to gitignore

--- run_pyDeltaRCM.py
+++ run_pyDeltaRCM.py
@@ -1,10 +1,10 @@
-from pyDeltaRCM import pyDeltaRCM
+from pyDeltaRCM.deltaRCM_driver import pyDeltaRCM
 
 if __name__ == '__main__':
 
     delta = pyDeltaRCM('deltaRCM.yaml')
 
-    for time in range(0,5000):
+    for time in range(0,1):
         delta.update()
 
     delta.finalize()
