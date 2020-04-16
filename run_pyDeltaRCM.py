87d3915d2be03f4a63453b802d7dfc4ac3f2babb
Eric Barefoot <eabarefoot@gmail.com>
2020-04-16 12:21:17 -0500

    feat: configure scripts to execute model with and without BMI
    We would like to see that the model can run alone and also with a BMI 
    interface, so I wrote a new script which runs the model as if it were a 
    normal model, and then also as a BMI model.

--- run_pyDeltaRCM.py
+++ run_pyDeltaRCM.py
@@ -1,14 +1,10 @@
-from pyDeltaRCM import BmiDelta
-import numpy as np
+from pyDeltaRCM import pyDeltaRCM
 
 if __name__ == '__main__':
-    
-    delta = BmiDelta()
-    
-    delta.initialize('deltaRCM.yaml')
-    
+
+    delta = pyDeltaRCM('deltaRCM.yaml')
+
     for time in range(0,5000):
-        print('Time:', delta.get_current_time())
         delta.update()
-        
+
     delta.finalize()
