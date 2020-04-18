a6f5c435e37ff7bbfa85a13c9a9162a0a14f8bac
deltarcm-helper <deltarcm-helper@users.noreply.github.com>
2020-04-18 13:27:24 -0500

    pep8 tests

--- tests/test_water_tools.py
+++ tests/test_water_tools.py
@@ -1,10 +1,11 @@
-## unit tests for water_tools.py
+# unit tests for water_tools.py
 
 import pytest
 
-import sys, os
+import sys
+import os
 import numpy as np
-sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
 
 from pyDeltaRCM import BmiDelta
 from pyDeltaRCM import water_tools
@@ -20,61 +21,68 @@ delta.initialize(os.getcwd() + '/tests/test.yaml')
 # now that it is initiated can access the water_tools via the inherited object
 # delta._delta.**water_tools_function**
 
+
 def test_update_flow_field_inlet():
-    '''
+    """
     Check that the flow at the inlet is set as expected
-    '''
+    """
     delta._delta.update_flow_field(1)
-    assert delta._delta.qw[0,4] == 1.
+    assert delta._delta.qw[0, 4] == 1.
+
 
 def test_update_flow_field_out():
-    '''
+    """
     Check that the flow in domain is set as expected when no flow (qx & qy==0)
-    '''
+    """
     delta._delta.update_flow_field(1)
-    assert delta._delta.qw[0,0] == 0.
+    assert delta._delta.qw[0, 0] == 0.
+
 
 def test_update_velocity_field():
-    '''
+    """
     Check that flow velocity field is updated as expected
-    '''
+    """
     delta._delta.update_velocity_field()
-    assert delta._delta.uw[0,0] == 0.
+    assert delta._delta.uw[0, 0] == 0.
+
 
 def test_pad_stage():
-    '''
+    """
     Test padding of stage field
     Padded shape will be initial length/width + 2 so [12,12]
-    '''
+    """
     delta._delta.init_water_iteration()
-    [a,b] = np.shape(delta._delta.pad_stage)
+    [a, b] = np.shape(delta._delta.pad_stage)
     assert a == 12
+
 
 def test_pad_depth():
-    '''
+    """
     Test padding of depth field
     Padded shape will be initial length/width + 2 so [12,12]
-    '''
+    """
     delta._delta.init_water_iteration()
-    [a,b] = np.shape(delta._delta.pad_depth)
+    [a, b] = np.shape(delta._delta.pad_depth)
     assert a == 12
+
 
 def test_pad_cell_type():
-    '''
+    """
     Test padding of cell_type field
     Padded shape will be initial length/width + 2 so [12,12]
-    '''
+    """
     delta._delta.init_water_iteration()
-    [a,b] = np.shape(delta._delta.pad_cell_type)
+    [a, b] = np.shape(delta._delta.pad_cell_type)
     assert a == 12
 
+
 def test_calculate_new_ind():
-    '''
+    """
     Test for function water_tools.calculate_new_ind
-    '''
+    """
     # assign old index
-    old_ind = [0,4]
+    old_ind = [0, 4]
     # assign new cell location
     new_cell = 7
     # expect new cell to be in location (1,4) -> 14
-    assert delta._delta.calculate_new_ind(old_ind,new_cell) == 14
+    assert delta._delta.calculate_new_ind(old_ind, new_cell) == 14
