d01c6b415621924d2181e7610979b0ff63c5497b
Eric Barefoot <eabarefoot@gmail.com>
2020-04-23 09:58:27 -0500

    fix: bug reported by @amoodie about path to default file.

--- pyDeltaRCM/deltaRCM_driver.py
+++ pyDeltaRCM/deltaRCM_driver.py
@@ -34,9 +34,8 @@ class pyDeltaRCM(Tools):
         self._time_step = 1.
 
         self.verbose = False
-        self.input_file = input_file
-        self.default_file = os.path.join(
-            os.getcwd(), 'pyDeltaRCM', 'default.yml')
+        self._file_dir = os.path.realpath(os.path.dirname(__file__))
+        self.default_file = os.path.join(self._file_dir, 'default.yml')
 
         self.import_files()
 
