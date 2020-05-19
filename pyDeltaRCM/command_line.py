572efb58f1adb9cf55e3a176e3b00469e8724430
Eric Barefoot <eabarefoot@gmail.com>
2020-05-19 15:06:30 -0500

    fix: some bugs and style changes
    @amoodie pointed out some issues with the PR. changed some importing 
    code, and one order of operations problem.

--- pyDeltaRCM/command_line.py
+++ pyDeltaRCM/command_line.py
@@ -1,8 +1,8 @@
 import os
 import argparse
 
-import pyDeltaRCM as deltaModule
-from pyDeltaRCM.deltaRCM_driver import pyDeltaRCM
+import pyDeltaRCM
+from . import deltaRCM_driver
 
 
 def run_model():
@@ -10,23 +10,22 @@ def run_model():
     parser = argparse.ArgumentParser(description='Options for running pyDeltaRCM from command line')
 
     parser.add_argument('--config', help='Path to a config file that you would like to use.')
-    parser.add_argument('--version', action='version', version='pyDeltaRCM ' + deltaModule.__version__, help='Prints the version of pyDeltaRCM.')
+    parser.add_argument('--version', action='version', version='pyDeltaRCM ' + pyDeltaRCM.__version__, help='Prints the version of pyDeltaRCM.')
 
     args = parser.parse_args()
 
     cmdlineargs = vars(args)
 
     if cmdlineargs['config'] is not None:
-        delta = pyDeltaRCM(input_file=cmdlineargs['config'])
+        delta = deltaRCM_driver.pyDeltaRCM(input_file=cmdlineargs['config'])
     else:
-        delta = pyDeltaRCM()
+        delta = deltaRCM_driver.pyDeltaRCM()
 
     for time in range(0, delta.timesteps):
 
-        print(delta._time)
         delta.update()
 
-        delta.finalize()
+    delta.finalize()
 
 
 if __name__ == '__main__':
