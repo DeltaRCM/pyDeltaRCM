607cfe6705214057be36c38e52ba25ed3b809f37
Andrew Moodie <amoodie@rice.edu>
2020-05-25 16:10:47 -0500

    delta object should be instantiated within each test for any consistent output checking.

--- tests/test_consistent.py
+++ tests/test_consistent.py
@@ -9,15 +9,15 @@ import numpy as np
 from pyDeltaRCM.deltaRCM_driver import pyDeltaRCM
 
 # need to create a simple case of pydeltarcm object to test these functions
-delta = pyDeltaRCM(input_file=os.path.join(os.getcwd(), 'tests', 'test.yaml'))
 
 
 def test_bed_after_one_update():
     """
     """
+    delta = pyDeltaRCM(input_file=os.path.join(os.getcwd(), 'tests', 'test.yaml'))
     delta.update()
     # slice is: delta.eta[:5, 4]
-    print(delta.eta[:5, 4])
+    # print(delta.eta[:5, 4])
 
-    _exp = np.array([-1., -0.82951754, -0.9981775 , -1., -1.])
+    _exp = np.array([-1., -0.840265, -0.9976036, -1., -1.])
     assert np.all(delta.eta[:5, 4] == pytest.approx(_exp))
