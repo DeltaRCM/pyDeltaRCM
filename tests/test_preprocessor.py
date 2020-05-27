5288da41bf8d1ffebc6c4ab7b7872508b592fc2b
Andrew Moodie <amoodie@rice.edu>
2020-05-27 17:59:48 -0500

    change entry point name

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -11,24 +11,13 @@ import pyDeltaRCM as _pyimportedalias
 from pyDeltaRCM import preprocessor
 
 
-def test_call():
+def test_entry_point_installed_call():
     """
     test calling the command line feature with a config file.
     """
-    subprocess.run(['run_pyDeltaRCM', '--config', os.path.join(os.getcwd(), 'tests', 'test_output.yaml')])
+    subprocess.run(['pyDeltaRCM', '--config', os.path.join(os.getcwd(), 'tests', 'test_output.yaml')])
     assert os.path.isfile(os.path.join(os.getcwd(), 'test', 'eta_0.0.png'))
     shutil.rmtree(os.path.join(os.getcwd(), 'test'))
-
-
-def test_version_call():
-    """
-    test calling the command line feature to query the version.
-    """
-    encoding = locale.getpreferredencoding()
-    printed1 = subprocess.run(['run_pyDeltaRCM', '--version'], stdout=subprocess.PIPE, encoding=encoding)
-    assert printed1.stdout == _pyimportedalias.__version__ + '\n'
-    printed2 = subprocess.run(['python', '-m', 'pyDeltaRCM', '--version'], stdout=subprocess.PIPE, encoding=encoding)
-    assert printed2.stdout == _pyimportedalias.__version__ + '\n'
 
 
 def test_python_call():
@@ -38,3 +27,15 @@ def test_python_call():
     subprocess.run(['python', '-m', 'pyDeltaRCM', '--config', os.path.join(os.getcwd(), 'tests', 'test_output.yaml')])
     assert os.path.isfile(os.path.join(os.getcwd(), 'test', 'eta_0.0.png'))
     shutil.rmtree(os.path.join(os.getcwd(), 'test'))
+
+
+def test_version_call():
+    """
+    test calling the command line feature to query the version.
+    """
+    encoding = locale.getpreferredencoding()
+    printed1 = subprocess.run(['pyDeltaRCM', '--version'], stdout=subprocess.PIPE, encoding=encoding)
+    assert printed1.stdout == _pyimportedalias.__version__ + '\n'
+    printed2 = subprocess.run(['python', '-m', 'pyDeltaRCM', '--version'], stdout=subprocess.PIPE, encoding=encoding)
+    assert printed2.stdout == _pyimportedalias.__version__ + '\n'
+
