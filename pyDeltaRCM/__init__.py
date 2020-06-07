9e522d29c069a4cf95ab52dbe26f5773cf556c95
Andrew Moodie <amoodie@rice.edu>
2020-06-07 16:18:03 -0400

    add python api Preprocessor to top level imports

--- pyDeltaRCM/__init__.py
+++ pyDeltaRCM/__init__.py
@@ -1,6 +1,7 @@
 from .model import DeltaModel
+from .preprocessor import Preprocessor
 from .bmi_delta import BmiDelta
 from .shared_tools import _get_version
 
-__all__ = ['DeltaModel', 'BmiDelta']
+__all__ = ['DeltaModel', 'Preprocessor', 'BmiDelta']
 __version__ = _get_version()
