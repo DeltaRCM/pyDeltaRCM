27a2a677405cb1c8c36a3482e5b583b8b100bff3
JayH <jayaram.hariharan@utexas.edu>
2020-08-25 22:51:05 -0400

    checkpointing tests

--- tests/test_consistent.py
+++ tests/test_consistent.py
@@ -5,6 +5,7 @@ import pytest
 import sys
 import os
 import numpy as np
+from netCDF4 import Dataset
 
 from pyDeltaRCM import DeltaModel
 
@@ -176,3 +177,293 @@ def test_limit_inds_error_fixed_bug_example_3(tmp_path):
 
     _exp = np.array([-4.99961, -4.605685, -3.8314152, -4.9007816, -5.])
     assert np.all(delta.eta[:5, 2] == pytest.approx(_exp))
+
+
+def test_model_similarity(tmp_path):
+    """Test consistency of two models initialized from same yaml."""
+    file_name = 'base_run.yaml'
+    p, f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(f, 'Length', 10.0)
+    utilities.write_parameter_to_file(f, 'Width', 10.0)
+    utilities.write_parameter_to_file(f, 'seed', 0)
+    utilities.write_parameter_to_file(f, 'dx', 1.0)
+    utilities.write_parameter_to_file(f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(f, 'Np_water', 10)
+    utilities.write_parameter_to_file(f, 'u0', 1.0)
+    utilities.write_parameter_to_file(f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(f, 'h0', 1.0)
+    utilities.write_parameter_to_file(f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(f, 'save_dt', 50)
+    utilities.write_parameter_to_file(f, 'save_strata', True)
+    f.close()
+
+    # create and update first model
+    ModelA = DeltaModel(input_file=p)
+    ModelA.update()
+    # create and update second model
+    ModelB = DeltaModel(input_file=p)
+    ModelB.update()
+
+    # fields should be the same
+    assert ModelA.time == ModelB.time
+    assert ModelA._time_iter == ModelB._time_iter
+    assert ModelA._save_iter == ModelB._save_iter
+    assert ModelA._save_time_since_last == ModelB._save_time_since_last
+    assert np.all(ModelA.uw == ModelB.uw)
+    assert np.all(ModelA.ux == ModelB.ux)
+    assert np.all(ModelA.uy == ModelB.uy)
+    assert np.all(ModelA.depth == ModelB.depth)
+    assert np.all(ModelA.stage == ModelB.stage)
+    assert np.all(ModelA.strata_eta.todense() ==
+                  ModelB.strata_eta.todense())
+    assert np.all(ModelA.strata_sand_frac.todense() ==
+                  ModelB.strata_sand_frac.todense())
+
+
+def test_simple_checkpoint(tmp_path):
+    """Test checkpoint vs a base run, and against another checkpoint run."""
+    # define a yaml for the longer model run
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 600)
+    base_f.close()
+    longModel = DeltaModel(input_file=base_p)
+
+    for _ in range(0, 3):
+        longModel.update()
+
+    # try defining a new model but plan to load checkpoint from longModel
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 600)
+    utilities.write_parameter_to_file(base_f, 'resume_checkpoint', True)
+    base_f.close()
+    resumeModel = DeltaModel(input_file=base_p)
+
+    # advance it one step to catch up to longModel
+    resumeModel.update()
+
+    # the longModel and resumeModel should match
+    assert longModel.time == resumeModel.time
+    assert np.all(longModel.uw == resumeModel.uw)
+    assert np.all(longModel.ux == resumeModel.ux)
+    assert np.all(longModel.uy == resumeModel.uy)
+    assert np.all(longModel.depth == resumeModel.depth)
+    assert np.all(longModel.stage == resumeModel.stage)
+    assert np.all(longModel.strata_eta.todense() ==
+                  resumeModel.strata_eta.todense())
+    # assert np.all(longModel.strata_sand_frac.todense() ==
+    #               resumeModel.strata_sand_frac.todense())
+
+    # define another model that loads the checkpoint
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 600)
+    utilities.write_parameter_to_file(base_f, 'resume_checkpoint', True)
+    base_f.close()
+    resumeModel2 = DeltaModel(input_file=base_p)
+
+    # advance it one step to catch up to resumeModel
+    resumeModel2.update()
+
+    # the two models that resumed from the checkpoint should be the same
+    assert resumeModel2.time == resumeModel.time
+    assert np.all(resumeModel2.uw == resumeModel.uw)
+    assert np.all(resumeModel2.ux == resumeModel.ux)
+    assert np.all(resumeModel2.uy == resumeModel.uy)
+    assert np.all(resumeModel2.depth == resumeModel.depth)
+    assert np.all(resumeModel2.stage == resumeModel.stage)
+    assert np.all(resumeModel2.strata_eta.todense() ==
+                  resumeModel.strata_eta.todense())
+    assert np.all(resumeModel2.strata_sand_frac.todense() ==
+                  resumeModel.strata_sand_frac.todense())
+
+
+def test_longer_checkpoint(tmp_path):
+    """Test checkpoint with longer run."""
+    # define a yaml for the longer model run
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 1200)
+    base_f.close()
+    longModel = DeltaModel(input_file=base_p)
+
+    for _ in range(0, 7):
+        longModel.update()
+
+    # try defining a new model but plan to load checkpoint from longModel
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 1200)
+    utilities.write_parameter_to_file(base_f, 'resume_checkpoint', True)
+    base_f.close()
+    resumeModel = DeltaModel(input_file=base_p)
+
+    # advance it three steps to catch up to longModel
+    for _ in range(0, 3):
+        resumeModel.update()
+
+    # the longModel and resumeModel should match
+    assert longModel.time == resumeModel.time
+    assert np.all(longModel.uw == resumeModel.uw)
+    assert np.all(longModel.ux == resumeModel.ux)
+    assert np.all(longModel.uy == resumeModel.uy)
+    assert np.all(longModel.depth == resumeModel.depth)
+    assert np.all(longModel.stage == resumeModel.stage)
+    assert np.all(longModel.strata_eta.todense() ==
+                  resumeModel.strata_eta.todense())
+    # assert np.all(longModel.strata_sand_frac.todense() ==
+    #               resumeModel.strata_sand_frac.todense())
+
+
+def test_checkpoint_nc(tmp_path):
+    """Test the netCDF that is written to by the checkpointing."""
+    # define a yaml for the base model run
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_eta_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_depth_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_discharge_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'checkpoint_dt', 600)
+    utilities.write_parameter_to_file(base_f, 'out_dir', tmp_path / 'test')
+    base_f.close()
+    baseModel = DeltaModel(input_file=base_p)
+
+    for _ in range(0, 4):
+        baseModel.update()
+
+    # try defining a new model but plan to load checkpoint from baseModel
+    file_name = 'base_run.yaml'
+    base_p, base_f = utilities.create_temporary_file(tmp_path, file_name)
+    utilities.write_parameter_to_file(base_f, 'Length', 10.0)
+    utilities.write_parameter_to_file(base_f, 'Width', 10.0)
+    utilities.write_parameter_to_file(base_f, 'seed', 0)
+    utilities.write_parameter_to_file(base_f, 'dx', 1.0)
+    utilities.write_parameter_to_file(base_f, 'L0_meters', 1.0)
+    utilities.write_parameter_to_file(base_f, 'Np_water', 10)
+    utilities.write_parameter_to_file(base_f, 'u0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'N0_meters', 2.0)
+    utilities.write_parameter_to_file(base_f, 'h0', 1.0)
+    utilities.write_parameter_to_file(base_f, 'SLR', 0.001)
+    utilities.write_parameter_to_file(base_f, 'Np_sed', 10)
+    utilities.write_parameter_to_file(base_f, 'save_dt', 50)
+    utilities.write_parameter_to_file(base_f, 'save_strata', True)
+    utilities.write_parameter_to_file(base_f, 'save_eta_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_depth_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_discharge_grids', True)
+    utilities.write_parameter_to_file(base_f, 'save_checkpoint', False)
+    utilities.write_parameter_to_file(base_f, 'resume_checkpoint', True)
+    utilities.write_parameter_to_file(base_f, 'out_dir', tmp_path / 'test')
+    base_f.close()
+    resumeModel = DeltaModel(input_file=base_p)
+
+    # advance it six steps
+    for _ in range(0, 6):
+        resumeModel.update()
+
+    # assert that ouput netCDF4 exists
+    exp_path_nc = os.path.join(tmp_path / 'test', 'pyDeltaRCM_output.nc')
+    assert os.path.isfile(exp_path_nc)
+
+    # load it into memory and check values in the netCDF4
+    output = Dataset(exp_path_nc, 'r', allow_pickle=True)
+    out_vars = output.variables.keys()
+    # check that expected variables are in the file
+    assert 'x' in out_vars
+    assert 'y' in out_vars
+    assert 'time' in out_vars
+    assert 'eta' in out_vars
+    assert 'depth' in out_vars
+    assert 'discharge' in out_vars
+    # check attributes of variables
+    assert output['time'][0].tolist() == 300.0
+    assert output['time'][-1].tolist() == 3000.0
+    assert output['eta'][0].shape == (10, 10)
+    assert output['eta'][-1].shape == (10, 10)
+    assert output['depth'][-1].shape == (10, 10)
+    assert output['discharge'][-1].shape == (10, 10)
