91ab4d340cd8f93a9db4463cbfc4898410315762
amoodie <andmoo22@gmail.com>
2021-04-03 01:11:08 +0000

    Deploying to gh-pages from  @ 87b557dec2edc53ed3c4173c8654b3bd6768282b 🚀

---
+++ guides/user_guide-1.py
@@ -0,0 +1,2 @@
+with pyDeltaRCM.shared_tools._docs_temp_directory() as output_dir:
+    mdl = pyDeltaRCM.DeltaModel(out_dir=output_dir)
---
+++ guides/user_guide-2.py
@@ -0,0 +1,3 @@
+fig, ax = plt.subplots(figsize=(4, 3))
+ax.imshow(mdl.bed_elevation, vmax=-3)
+plt.show()
