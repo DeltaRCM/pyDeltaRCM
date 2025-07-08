062baac12db182c6fdd0de5d6b163a0985e052f6
Andrew Moodie <amoodie@utexas.edu>
2025-07-08 11:46:23 -0500

    improve error message around water weights summing to zero and issues with gamma. Update to pull gramma private variable directly when needed.

--- pyDeltaRCM/water_tools.py
+++ pyDeltaRCM/water_tools.py
@@ -406,7 +406,7 @@ class water_tools(abc.ABC):
             self.jvec_flat,
             self.distances_flat,
             self.dry_depth,
-            self.gamma,
+            self._gamma,
             self._theta_water,
         )
 
@@ -486,6 +486,8 @@ class water_tools(abc.ABC):
         self.qyn *= qwn_div
 
         if self._time_iter > 0:
+            # On first iteration, use the set omega_flow parameter to combine
+            #   old qs with new qs. Then, use a factor based on total number of iterations
             omega = self.omega_flow_iter
             if iteration == 0:
                 omega = self._omega_flow
@@ -697,7 +699,10 @@ def _get_weight_at_cell_water(
             weight = weight / np.sum(weight)
 
     else:
-        raise RuntimeError("Water sum(weight) less than 0. " "Please report error.")
+        raise RuntimeError(
+            "Water sum(weight) less than 0. This is common when the value of gamma is "
+            "too large. See documentation for complete explanation."
+        )
 
     # final sanity check
     if np.any(np.isnan(weight)):
