4902a9f4387c01ae2f17223a6ed9ea5cd32b8f68
Andrew Moodie <andmoo22@gmail.com>
2026-02-18 08:20:45 -0600

    hotfix _version.py

--- pyDeltaRCM/_version.py
+++ pyDeltaRCM/_version.py
@@ -3,8 +3,4 @@ def __version__() -> str:
     Private version declaration, gets assigned to pyDeltaRCM.__version__
     during import
     """
-<<<<<<< sandsuet_output
     return "2.2.0"
-=======
-    return "2.1.10"
->>>>>>> develop
