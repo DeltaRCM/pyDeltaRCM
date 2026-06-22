2411266df7c2f145accbccaf9c7e1b94b4365516
Andrew Moodie <amoodie@tamu.edu>
2026-06-22 15:50:28 -0500

    update example including hints and references to other useful parameters (incl force_deposit).

--- docs/source/examples/verify_mass_conservation.py
+++ docs/source/examples/verify_mass_conservation.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import pyDeltaRCM
 
+
 class MassConservationCheck(pyDeltaRCM.DeltaModel):
 
     def __init__(self, input_file, **kwargs):
@@ -10,13 +11,51 @@ class MassConservationCheck(pyDeltaRCM.DeltaModel):
 
         self.inlets_flat = self.inlet
 
+        # trackers for cumulative volume changes
         self.cumulative_input_volume = 0
         self.cumulative_lost_volume = 0
         self.cumulative_exported_volume = 0
+        self.cumulative_inlet_volume = 0
 
-        print(f"|" + 66*"-" + "|")
-        print(f"| vol input     | vol deposit   | % err    | % lost   | % exported | ")
-        print(f"|" + 66*"-" + "|")
+        # some code to help format a nice table
+        self._fields = [
+            f"timestep",
+            f"vol input",
+            f"vol deposit",
+            f"% err",
+            f"% lost",
+            f"% exported",
+            f"% at inlet",
+        ]
+        self._sty0 = ["g", ".1e", ".1e", ".2f", ".2f", ".2f", ".2f"]
+        self._fields_len = [
+            np.maximum(len(s), 8) for s in self._fields
+        ]  # field plus two spaces on each side
+        self._tot_len = (
+            np.sum(self._fields_len)
+            + 2
+            + (len(self._fields) - 1)
+            + 2
+            + 2 * (len(self._fields) - 1)
+        )  # fields + end |s + middle |s + end spcs + middle spcs
+
+        # print table header
+        self._format_table()
+        self._format_table(self._fields)
+
+    def _format_table(self, entry=None):
+
+        if entry is None:
+            print(f"|" + ((self._tot_len - 2) * "-") + "|")
+        else:
+            fl = self._fields_len
+            sty0 = self._sty0
+            # make list of stings
+            row = []
+            for i in np.arange(len(entry)):
+                styi = sty0[i] if not isinstance(entry[i], str) else ""
+                row.append(f"{entry[i]:>{fl[i]}{styi}}")
+            print("| " + " | ".join(row) + " |")
 
     def hook_after_finalize_timestep(self):
         self._verify_mass_conservation()
@@ -33,52 +72,74 @@ class MassConservationCheck(pyDeltaRCM.DeltaModel):
         total amount of landscape change (volume) is close to the total
         amount of sediment put into the domain.
         """
-
-        # check the change in volume of the deposit for this timestep
-        act_deposit_volume = np.sum((self.eta - self.eta_init) * self.dx**2)
+        # calculate change in input sediment and deposit volume
         input_sed_volume = self.Qs0 * self.dt
+        act_deposit_volume = np.sum((self.eta - self.eta_init) * self.dx**2)
 
-        # now check it accounting for whatever was lost or exported
-        exported_volume = self._sr.Vp_exported
-        lost_volume = self._sr.Vp_lost
-        
+        # changes in input, lost, exported, inlet
+        lost_volume = self._sr.Vp_lost + self._mr.Vp_lost
+        exported_volume = self._sr.Vp_exported + self._mr.Vp_exported
+        inlet_volume = self._Vp_inletbc
+
+        # accumulate
+        self.cumulative_input_volume += input_sed_volume
         self.cumulative_lost_volume += lost_volume
         self.cumulative_exported_volume += exported_volume
-        self.cumulative_input_volume += input_sed_volume
+        self.cumulative_inlet_volume += inlet_volume
 
-        cumulative_lost_frac_error = self.cumulative_lost_volume / self.cumulative_input_volume
-        cumulative_exported_frac_error = self.cumulative_exported_volume / self.cumulative_input_volume
+        cumulative_lost_frac_error = (
+            self.cumulative_lost_volume / self.cumulative_input_volume
+        )
+        cumulative_exported_frac_error = (
+            self.cumulative_exported_volume / self.cumulative_input_volume
+        )
+        cumulative_inlet_frac_error = (
+            self.cumulative_inlet_volume / self.cumulative_input_volume
+        )
+        cumulative_deposit_frac_error = (
+            act_deposit_volume - self.cumulative_input_volume
+        ) / self.cumulative_input_volume
 
-        deposit_volume_error = (act_deposit_volume - self.cumulative_input_volume) / self.cumulative_input_volume
-
-        print(f"|{self.cumulative_input_volume:>15.1e}|{act_deposit_volume:>15.2e}|{deposit_volume_error*100:>10.2f}|{cumulative_lost_frac_error*100:>10.2f}|{cumulative_exported_frac_error*100:>12.2f}| ")
+        entry = [
+            self._time_iter,
+            self.cumulative_input_volume,
+            act_deposit_volume,
+            cumulative_deposit_frac_error * 100,
+            cumulative_lost_frac_error * 100,
+            cumulative_exported_frac_error * 100,
+            cumulative_inlet_frac_error * 100,
+        ]
+        self._format_table(entry)
 
     def finalize(self):
-        super().finalize()
-        print(f"|" + 66*"-" + "|")
-        print("")
+        super().finalize()  # .finalize() from base model
+        # print table footer
+        self._format_table()
+        print("")  # blankline
 
 
 if __name__ == "__main__":
-    
-    demo_conditions = dict(f_bedload=1, Np_sed=200, itermax=1)
+
+    demo_conditions = dict(Np_sed=200, itermax=1)
     # normal conditions yield good balance
-    print("A good balance yields:")
+    print("Default parameters yield cumulative volume conservation errors (%):")
     model = MassConservationCheck(input_file=None, **demo_conditions)
     for _ in np.arange(10):
         model.update()
     model.finalize()
 
     # too small of stepmax causes many "lost" partciles
-    print("Too small of stepmax yields:")
+    print("Too small of stepmax yields cumulative volume conservation errors (%):")
     model = MassConservationCheck(input_file=None, **demo_conditions, stepmax=5)
     for _ in np.arange(10):
         model.update()
     model.finalize()
 
     # too small of domain causes many "exported" partciles
-    print("Too small of domain yields:")
-    model = MassConservationCheck(input_file=None, **demo_conditions, Length=500, Width=1000)
+    print("Too small of domain yields cumulative volume conservation errors (%):")
+    model = MassConservationCheck(
+        input_file=None, **demo_conditions, Length=500, Width=1000
+    )
     for _ in np.arange(10):
         model.update()
     model.finalize()
